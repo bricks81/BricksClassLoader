9001ba57946b3ac35bec2ece24a6e3f17df5a7c9
Sven Ullmann <sven.ullmann@e3n.de>
2015-08-23 21:37:28 +0200

    arranging things

--- test/config/module.config.php
+++ test/config/module.config.php
@@ -1,42 +1,42 @@
 <?php
 
 return array(
-	'BricksClassLoader' => array(
-		'BricksClassLoader' => array(
-			'BricksClassLoader' => array(
-				'classLoaderClass' => 'Bricks\ClassLoader\ClassLoader',
-				'defaultClassLoaderClass' => 'Bricks\ClassLoader\DefaultClassLoader',
-				'defaultInstantiator' => 'Bricks\ClassLoader\DefaultInstantiator',
-				'defaultFactory' => 'Bricks\ClassLoader\DefaultFactory',
+	'BricksConfig' => array(		
+		'BricksClassLoader' => array( // Module to configure
+			'BricksClassLoader' => array( // Namespace
 				'defaultFactories' => array(),
-			),
-		),
-		'BricksClassLoaderTest' => array(
-			'BrickClassLoaderTest' => array(
-				'anyClass' => 'BricksClassLoaderTest\TestObject',
-				'anyClassExtended' => 'BricksClassLoaderTest\TestObjectExtended',
-				'deeper' => array(
-					'class' => array(
-						'hierarchy' => 'BricksClassLoaderTest\TestObject',
+				'classMap' => array(
+					'BricksClassLoader' => array( // Module to load from
+						'BricksClassLoader' => array( // Namespace
+							'classLoaderClass' => 'Bricks\ClassLoader\ClassLoader',
+							'defaultClassLoaderClass' => 'Bricks\ClassLoader\DefaultClassLoader',
+							'defaultInstantiator' => 'Bricks\ClassLoader\DefaultInstantiator',
+							'defaultFactory' => 'Bricks\ClassLoader\DefaultFactory',
+							
+						),
+					),
+					'BricksClassLoaderTest' => array(
+						'BricksClassLoaderTest' => array(
+							'anyClass' => 'BricksClassLoaderTest\TestObject',
+							'anyClassExtended' => 'BricksClassLoaderTest\TestObjectExtended',
+							'deeper' => array(
+								'class' => array(
+									'hierarchy' => 'BricksClassLoaderTest\TestObject',
+								),
+							),
+						),
+					),
+					'BricksClassLoaderTest2' => array(
+						'BricksClassLoaderTest\TestObjectExtended' => 'BricksClassLoaderTest\TestObjectExtended2',
+						'BricksClassLoaderTest\TestObject' => 'BricksClassLoaderTest\TestObject2',
+						'deeper' => array(
+							'class' => array(
+								'hierarchy' => 'BricksClassLoaderTest\TestObject2',
+							),
+						),
 					),
 				),
-			),		
-			'BricksClassLoaderTest2' => array(
-				'BricksClassLoaderTest\TestObjectExtended' => 'BricksClassLoaderTest\TestObjectExtended2',
-				'BricksClassLoaderTest\TestObject' => 'BricksClassLoaderTest\TestObject2',
-				'deeper' => array(
-					'class' => array(
-						'hierarchy' => 'BricksClassLoaderTest\TestObject2',
-					),
-				),
 			),
 		),
-	),
-	'BricksConfig' => array(
-		'BricksClassLoader' => array(
-			'BricksClassLoader' => array(				
-				
-			),	
-		),		
-	),
+	),	
 );
